4766614ff59bf12748840d643d05593d21cd68c5
Скваж Сергей <skvazhsm@petrozavodsk-mo.prv>
2024-03-01 17:00:36 +0300

    test translite controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 
 use Illuminate\View\View;
 
+use Illuminate\Support\Str;
+
 
 class TestController extends Controller
 {
@@ -18,61 +20,73 @@ class TestController extends Controller
     public function index(Request $request)
     {
 
-        // $user = User::find(1);
-        // return view('test', [
-        //     'user' => $user,
-        // ]);
 
         $KIVS   = config('kivs.KIVS');
 
         print_r($KIVS);
+
         $users = [
             "Скваж Сергей Михайлович",
             "Васильев Сергей Дмитриевич",
-            "Алёшин Александр Сергеевич",
+            "Алешин Александр Сергеевич",
             "Бабич Владимир Владимирович",
             "Сивко Лиана Зурабовна",
             "Матюшина Лилия Васильевна"
         ];
 
 
-        foreach ($users as $user) {
-            # code...
-            echo "<br>";
-            print_r($user);
-            $tmp = explode(" ", $user);
-            $fname      = $tmp[0];
-            $name       = $tmp[1];
-            $sname      = $tmp[2];
+        foreach($users as $user) {
 
+            echo "<br>",$user;
 
-            print_r($tmp);
+            $tmp    = explode(" ", $user);
+            $fname  = $tmp[0];
+            $name   = $tmp[1];
+            $sname  = $tmp[2];
+
+            // foreach (mb_str_split($user) as $char)
+            //     echo "  >",$KIVS[$char];
+            // }
+
+            // print_r($tmp);
             $fname_lat = "";
-            for($i=0; $i<strlen($fname); $i++){
-                // $fname_lat .= $KIVS[$fname[$i]];
-                print("  ");
-                print_r($fname[$i]);
-                // print_r($KIVS[$fname[$i]]);
+            // for($i=0; $i<strlen($fname); $i++){
+            foreach (mb_str_split($fname) as $char){
+                $fname_lat .= $KIVS[$char];
+                // echo "  ",$char;
             }
 
-            /*
             $name_lat = "";
-            for($i=0; $i<strlen($name); $i++){
-                $name_lat .= $KIVS[$name[$i]];
+            // for($i=0; $i<strlen($name); $i++){
+            foreach (mb_str_split($name) as $char){
+                $name_lat .= $KIVS[$char];
             }
 
-            $aname_lat = "";
-            for($i=0; $i<strlen($sname); $i++){
-                $aname_lat .= $KIVS[$sname[$i]];
+            $sname_lat = "";
+            foreach (mb_str_split($sname) as $char){
+                $sname_lat .= $KIVS[$char];
             }
 
-            print_r($fname, $name, $sname);
+            $fname_lat  = Str::lower($fname_lat);
+            $name_lat   = Str::lower($name_lat);
+            $sname_lat  = Str::lower($sname_lat);
+
+            echo "<br>",$fname_lat, $name_lat, $sname_lat;
             // $users_lat  = [];
             // $fname_lat  = [];
             // $name_lat   = [];
             // $sname_lat  = [];
-*/
+
+            $login          = $fname_lat."".Str::of($name_lat)->charAt(0)."".Str::of($sname_lat)->charAt(0);
+            $email_login    = $name_lat.".".$fname_lat."";
+            $email          = $name_lat.".".$fname_lat."@petrozavodsk-mo.ru";
+            echo "<br>", $login;
+            echo "<br>", $email;
+            echo "<br>", $email_login;
+
 
         }
+
     }
+
 }
--- config/kivs.php
+++ config/kivs.php
@@ -1,5 +1,6 @@
 <?php
 
+// return["KIVS"=>"йцукенгшщзхъ"];
 return[
     "KIVS" => [
         " "=>" ",
